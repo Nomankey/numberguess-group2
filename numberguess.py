6a044e70a119cb061060b58e395babe22be439ea
Daye Park <dayepark@Dayeui-MacBookPro.local>
2021-01-29 16:49:43 +0900

    Solved: # 2
    feat: Add while statement in order to make  limitation of chances

--- numberguess.py
+++ numberguess.py
@@ -4,15 +4,18 @@ import random
 answer = random.randint(1,100)
 print('DEBUG: {}'.format(answer))
 
-guess = int(input('Guess the number(1-100): '))
-print(guess, type(guess))
+chance = 5 
 
-if guess-answer==0:
-    print('Correct!')
-elif guess>answer:
-    print('Down!')
-elif guess<answer:
-    print('Up!')
+while chance > 0:
+    guess = int(input('Guess the number(1-100): '))
+    print(guess, type(guess))
+
+   if guess-answer==0:
+        print('Correct!')
+   elif guess>answer:
+        print('Down!')
+    elif guess<answer:
+        print('Up!')
 
 
     
