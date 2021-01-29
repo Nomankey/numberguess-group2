c4d2839a26b7c0482c9bd710a9bb3146c840c220
Shawn CK.P <snowy213@outlook.com>
2021-01-29 16:12:37 +0900

    "feat: made a number game that get random number as an answer and input guess"

---
+++ numberguess.py
@@ -0,0 +1,14 @@
+import random
+
+
+answer = random.randint(1,100)
+print('DEBUG: {}'.format(answer))
+
+guess = int(input('Guess the number(1-100): '))
+print(guess, type(guess))
+
+if guess-answer==0:
+    print('Correct!')
+else:
+    print('Wrong! The answer was {}.'.format(answer))
+
