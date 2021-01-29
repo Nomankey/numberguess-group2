f51063a8900d1312deeb518ae53da72b462eba4c
Dojun Kim <dojunkim@Dojuns-MacBook-Pro.local>
2021-01-29 22:34:50 +0900

    feat: acquire username
    to make personalized sentences

--- numberguess.py
+++ numberguess.py
@@ -4,16 +4,14 @@ import random
 answer = random.randint(1,100)
 print('DEBUG: {}'.format(answer))
 
+username = input('What is your name? ')
 guess = int(input('Guess the number(1-100): '))
 print(guess, type(guess))
 
 if guess-answer==0:
-    print('Correct!')
+    print('Correct, {}!'.format(username))
 elif guess>answer:
-    print('Down!')
+    print('Down, {}!'.format(username))
 elif guess<answer:
-    print('Up!')
-
-
-    
+    print('Up, {}!'.format(username))
 
