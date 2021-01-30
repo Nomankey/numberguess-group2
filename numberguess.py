c069f6b9df113bc2dee13709e751a295d1a0cbd5
Daye Park <dayepark@Dayeui-MacBookPro.local>
2021-01-30 23:59:27 +0900

    Solved: #2
    feat: Add a feature that limits the number of chances to try
    
    also, added a sentence that informs that all the opportunities is used up

--- numberguess.py
+++ numberguess.py
@@ -4,11 +4,18 @@ import random
 answer = random.randint(1,100)
 print('DEBUG: {}'.format(answer))
 
-guess = int(input('Guess the number(1-100): '))
-print(guess, type(guess))
 
-if guess-answer==0:
-    print('Correct!')
-else:
-    print('Wrong! The answer was {}.'.format(answer))
+chance = 5                  # the number of chance is randomly chosen 
 
+while chance > 0:
+    guess = int(input('Guess the number(1-100): '))
+    print(guess, type(guess))
+
+    if guess-answer==0:
+        print('Correct!')
+        break
+    else:
+        print('Wrong! The answer was {}.'.format(answer))
+
+    if chance == 0:        # Add a sentence to be printed when the chance is all used up
+        print('Unfortunately, you've used up all the chances... Next time!')
